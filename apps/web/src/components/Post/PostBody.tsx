130d380e4542679fca76ceb9fda9a85e35493594
Paul Burke <paul@paulburke.co>
2025-12-09 10:05:04 -0500

    feat: blurs posts with content warnings

--- apps/web/src/components/Post/PostBody.tsx
+++ apps/web/src/components/Post/PostBody.tsx
@@ -1,15 +1,15 @@
 import { EyeIcon } from "@heroicons/react/24/outline";
 import getPostData from "@hey/helpers/getPostData";
 import { isRepost } from "@hey/helpers/postHelpers";
-import type { AnyPostFragment } from "@hey/indexer";
+import { type AnyPostFragment, ContentWarning } from "@hey/indexer";
 import { getSrc } from "@livepeer/react/external";
-import { memo } from "react";
+import { memo, useState } from "react";
 import Quote from "@/components/Shared/Embed/Quote";
 import Markup from "@/components/Shared/Markup";
 import Attachments from "@/components/Shared/Post/Attachments";
 import PostLink from "@/components/Shared/Post/PostLink";
 import Video from "@/components/Shared/Post/Video";
-import { H6 } from "@/components/Shared/UI";
+import { Button, H6 } from "@/components/Shared/UI";
 import cn from "@/helpers/cn";
 
 interface PostBodyProps {
@@ -47,34 +47,68 @@ const PostBody = ({
   // Show attachments if they're there
   const showAttachments = filteredAttachments.length > 0 || filteredAsset;
 
+  const [showCensored, setShowCensored] = useState(false);
+  const contentWarning =
+    "contentWarning" in metadata ? metadata.contentWarning : undefined;
+  const contentWarningText =
+    contentWarning === ContentWarning.Spoiler
+      ? "spoiler"
+      : contentWarning === ContentWarning.Nsfw
+        ? "NSFW content"
+        : contentWarning === ContentWarning.Sensitive
+          ? "sensitive content"
+          : "content";
+
   return (
-    <div className="break-words">
-      <Markup
-        className={cn(
-          { "line-clamp-5": canShowMore },
-          "markup linkify break-words",
-          contentClassName
-        )}
-        mentions={targetPost.mentions}
-      >
-        {content}
-      </Markup>
-      {canShowMore ? (
-        <H6 className="mt-4 flex items-center space-x-1 text-gray-500 dark:text-gray-200">
-          <EyeIcon className="size-4" />
-          <PostLink post={post}>Show more</PostLink>
-        </H6>
-      ) : null}
-      {/* Attachments and Quotes */}
-      {showAttachments ? (
-        <Attachments asset={filteredAsset} attachments={filteredAttachments} />
-      ) : null}
-      {showLive ? (
-        <div className="mt-3">
-          <Video src={getSrc(metadata.liveUrl || metadata.playbackUrl)} />
+    <div className="relative">
+      {contentWarning && !showCensored && (
+        <div className="absolute z-10 flex h-full w-full items-center justify-center">
+          <Button
+            onClick={(event) => {
+              event.stopPropagation();
+              setShowCensored(true);
+            }}
+            size="sm"
+          >
+            Show {contentWarningText}
+          </Button>
         </div>
-      ) : null}
-      {targetPost.quoteOf ? <Quote post={targetPost.quoteOf} /> : null}
+      )}
+      <div
+        className={cn("break-words", {
+          "opacity-50 blur-2xl": contentWarning && !showCensored
+        })}
+      >
+        <Markup
+          className={cn(
+            { "line-clamp-5": canShowMore },
+            "markup linkify break-words",
+            contentClassName
+          )}
+          mentions={targetPost.mentions}
+        >
+          {content}
+        </Markup>
+        {canShowMore ? (
+          <H6 className="mt-4 flex items-center space-x-1 text-gray-500 dark:text-gray-200">
+            <EyeIcon className="size-4" />
+            <PostLink post={post}>Show more</PostLink>
+          </H6>
+        ) : null}
+        {/* Attachments and Quotes */}
+        {showAttachments ? (
+          <Attachments
+            asset={filteredAsset}
+            attachments={filteredAttachments}
+          />
+        ) : null}
+        {showLive ? (
+          <div className="mt-3">
+            <Video src={getSrc(metadata.liveUrl || metadata.playbackUrl)} />
+          </div>
+        ) : null}
+        {targetPost.quoteOf ? <Quote post={targetPost.quoteOf} /> : null}
+      </div>
     </div>
   );
 };
